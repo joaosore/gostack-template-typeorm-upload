0aa14e161ef4bb74a1ee01c9be1e463d08cc1a7c
João Marcos <jaumcj@hotmail.com>
2021-01-06 15:52:31 -0300

    first commit

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,60 @@
+import AppError from '../errors/AppError';
+
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import TrasnsactionsRepository from '../repositories/TransactionsRepository';
+
+import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TrasnsactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transction);
+
+    return transction;
+  }
+}
+
+export default CreateTransactionService;
